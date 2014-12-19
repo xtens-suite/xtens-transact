9d6ec0e27ce672307b1b808a8d6a164c0d8ee093
zigur <massimorgon@gmail.com>
2014-12-19 17:57:56 +0100

    some tests added

--- test/KnexStrategy-test.js
+++ test/KnexStrategy-test.js
@@ -1,27 +1,25 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var KnexStrategy = require('./../lib/KnexStrategy.js');
+var FileSystemManager = require('xtens-fs').FileSystemManager;
+
 
 /* a test connection according to sails format */
-var connection = {
-    adapter: 'sails-postgresql',
-    host: 'localhost',
-    port: 8432,
-    user: 'userpg',
-    password: 'pwpg',
-    database: 'xtens',
-    pool: true,
-    ssl: false,
-    schema: true
-};
+var dbConnection = require('./../config/local.js').connection;
+var fsConnection = require('./../config/local.js').fsConnection;
 
 // test data object
 var dataObj = {
     files: [
         {uri: "/path/to/file01.ext"},
-        {name: "/another/path/to/file02.ext"},
+        {uri: "/another/path/to/file02.ext"},
         {uri: "/yet/another/path/to/file03.ext"}
     ],
-    type: 1,
+    type: {
+        id: 1,
+        name: 'testDataType',
+        schema: {}
+    },
     acquisitionDate: new Date(),
     tags: ["test", "knex test"],
     notes: "let me test you with knex",
@@ -39,7 +37,7 @@ describe('KnexStrategy', function() {
     describe('#constructor', function() {
 
         it ("should create a new knex object with the proper connection", function() {
-            var strategy = new KnexStrategy(connection);
+            var strategy = new KnexStrategy(dbConnection);
             expect(strategy.knex).to.exist;
             expect(strategy.knex).to.have.property('select');
             expect(strategy.knex).to.have.property('insert');
@@ -48,12 +46,14 @@ describe('KnexStrategy', function() {
 
     });
 
-    describe('#dataCreate', function() {
+    describe('#createData', function() {
 
-        var strategy = new KnexStrategy(connection);
+        var strategy = new KnexStrategy(dbConnection, fsConnection);
 
         it("# should create the proper query strategy", function() {
-        
+            var mock = sinon.mock(FileSystemManager.prototype);
+
+            strategy.createData(dataObj);
         });
     
     });
