3a29a4ae231210d6dd2cb121005cc4732f766977
zigur <massimorgon@gmail.com>
2014-12-27 13:13:34 +0100

    added exports

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author Massimiliano Izzo
+ */
+var KnexStrategy = require('./lib/KnexStrategy.js');
+var TransactionHandler = require('./lib/TransactionHandler.js');
+module.exports.KnexStrategy = KnexStrategy;
+module.exports.TransactionHandler = TransactionHandler;
+
