453ad1e7ac7254d022f8af26b8270d37d8517f1a
zigur <massimorgon@gmail.com>
2014-12-27 23:50:23 +0100

    Validation implemented & tested

--- test/Validation-test.js
+++ test/Validation-test.js
@@ -17,19 +17,95 @@ describe("Validation", function() {
                 tags: ["tag", "another tag"],
                 notes: "let me test you with knex",
                 metadata: {
-                    attribute1: { value: ["test value"]},
-                    attribute2: { value: [1.0], unit: ["s"]}
+                    "attribute1": { "value": ["test value"]},
+                    "attribute2": { "value": [1.0], "unit": ["s"]}
                 },
                 parentData: undefined,
                 parentSample: undefined,
                 parentSubject: 1
             };
             var errors = Validation.validateData(validData);
-            if (errors && errors.length) {
-                errors.forEach(function(error) { console.log(error); });
-            }
             expect(errors).to.be.null;
+            delete validData.files;
+            delete validData.acquisitionDate;
+            errors = Validation.validateData(validData);
+            expect(errors).to.be.null;
+            delete validData.tags;
+            errors = Validation.validateData(validData);
+            expect(errors).to.be.null;
+            
+        });
 
+        it("should return an error message for date not valid", function() {
+            var unformattedDateData = {
+                type: 2,
+                acquisitionDate: "11/122/198",
+                tags: ["tag", "another tag"],
+                notes: "let me test you with knex",
+                metadata: {
+                    "attribute1": { "value": ["test value"]},
+                    "attribute2": { "value": [1.0], "unit": ["s"]}
+                },
+                parentData: undefined,
+                parentSample: undefined,
+                parentSubject: 1
+            };
+            var errors = Validation.validateData(unformattedDateData);
+            expect(errors).to.have.length(1);
+            console.log(errors[0]);
+            expect(errors[0]).to.have.string(Validation.INVALID_DATE);
+        });
+
+        it("should return an error message for date not valid", function() {
+            var invalidTagsData = {
+                type: 2,
+                acquisitionDate: new Date(),
+                tags: "tag",
+                notes: "let me test you with knex",
+                metadata: {
+                    "attribute1": { "value": ["test value"]},
+                    "attribute2": { "value": [1.0], "unit": ["s"]}
+                },
+                parentData: undefined,
+                parentSample: undefined,
+                parentSubject: 1
+            };
+            var errors = Validation.validateData(invalidTagsData);
+            expect(errors).to.have.length(1);
+            console.log(errors[0]);
+            expect(errors[0]).to.have.string(Validation.INVALID_TAG);
+            invalidTagsData.tags = {1: "tag", 2: "another tag"};
+            errors = Validation.validateData(invalidTagsData);
+            expect(errors).to.have.length(1);
+            console.log(errors[0]);
+            expect(errors[0]).to.have.string(Validation.INVALID_TAG);
+
+        });
+        
+        it("should return an error message for missing and/or invalid metadata", function() {
+            var invalidMetadataData = {
+                type: 2,
+                acquisitionDate: new Date(),
+                tags: ["tag"],
+                notes: "let me test you with knex",
+                parentData: undefined,
+                parentSample: undefined,
+                parentSubject: 1
+            };
+            var errors = Validation.validateData(invalidMetadataData);
+            expect(errors).to.have.length(2);
+            console.log(errors[0]);
+            expect(errors[0]).to.have.string(Validation.MISSING_METADATA);
+            expect(errors[1]).to.have.string(Validation.INVALID_METADATA);
+            invalidMetadataData.metadata = {};
+            errors = Validation.validateData(invalidMetadataData);
+            console.log(errors[0]);
+            expect(errors).to.have.length(1);
+            expect(errors[0]).to.have.string(Validation.MISSING_METADATA);
+            invalidMetadataData.metadata = "metadata string";
+            errors = Validation.validateData(invalidMetadataData);
+            expect(errors).to.have.length(1);
+            expect(errors[0]).to.have.string(Validation.INVALID_METADATA);
         });
     });
 
